1694297e052750ab989a86f6416c4a9908eeab42
J Hershey <hersheyj32@gmail.com>
2019-03-13 13:42:09 -0700

    the migration doesn't work... boo

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -15,11 +15,12 @@ namespace MvcMovie.Models
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
-        public DateTime ReleaseDate { get; set;}
+        public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
 
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+        public string Rating { get; set; }
 
     }
 }
